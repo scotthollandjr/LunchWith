880674b85361371fbe08794c54e032fc9345054d
Scott Holland <scotthollandjr@gmail.com>
2016-08-25 09:19:58 -0700

    Add footer with tabs, not sticky yet. Floating with gap between bottom of footer and bottom of view

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -3,19 +3,24 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-          <footer className="footer">
+          <footer className="footer nav">
             <div className="container">
-              <div className="content has-text-centered">
-                <p>
-                  <strong>LunchWith</strong> by KW & SH. The source code is licensed
-                  <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                  is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
-                </p>
-                <p>
-                  <a className="icon" href="https://github.com/kylewolfson/shareabyte">
-                    <i className="fa fa-github"></i>
-                  </a>
-                </p>
+              <div className="nav-center">
+                <a class="nav-item is-tab is-active">
+                  <span className="icon is-large footer-icon">
+                    <i className="fa fa-home"></i>
+                  </span>
+                </a>
+                <a class="nav-item is-tab is-active">
+                  <span className="icon is-large footer-icon">
+                    <i className="fa fa-calendar-plus-o"></i>
+                  </span>
+                </a>
+                <a class="nav-item is-tab is-active">
+                  <span className="icon is-large footer-icon">
+                    <i className="fa fa-user"></i>
+                  </span>
+                </a>
               </div>
             </div>
           </footer>
