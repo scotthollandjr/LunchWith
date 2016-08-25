504831434e347c47075707d8393d645774c736fa
Scott Holland <scotthollandjr@gmail.com>
2016-08-25 12:44:07 -0700

    Add login page with meticulously styles linkedin button

---
+++ app/components/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Login extends React.Component {
+    render() {
+        return (
+          	<div>
+	          	<script type="in/Login"></script>
+
+	           	<a href="/account" className="button is-blue" id="linked-button">
+			  		<span className="icon linked-icon">
+			    		<i className="fa fa-linkedin"></i>
+			  		</span>
+			  		<span>Sign in with LinkedIn</span>
+				</a>
+          </div>
+        );
+    }
+};
+
+export default Login;
