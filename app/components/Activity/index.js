45502c05aafa803e8d83a9b94ebe7432873fba95
Scott Holland <scotthollandjr@gmail.com>
2016-08-31 13:03:33 -0700

    Get google map to appear in activity component

--- app/components/Activity/index.js
+++ app/components/Activity/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
-import MapComp from '../Map';
+import GoogleMap from '../GoogleMap';
 
 class Activity extends React.Component {
     render() {
         return (
           <div className="flex_container">
             <Header />
-            <div className="flex_body"></div>
+           	<GoogleMap />
             <Footer />
           </div>
         );
---
+++ app/components/GoogleMap/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+
+const coords = {
+	lat: 45.527129,
+	lng: -122.683163
+};
+
+const GoogleMap = React.createClass({
+	  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true
+    });
+  },
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  },
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  },
+
+  onClick(e) {
+    console.log('onClick', e);
+  },
+
+  render() {
+    return (
+      <Gmaps
+        width={'100vh'}
+        height={'100vh'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={12}
+        loadingMessage={'Be happy'}
+        params={{v: '3.exp', key: 'AIzaSyCJa4qHOKLW1eYexkJr2WLQ5I24xyqP-5E'}}
+        onMapCreated={this.onMapCreated}>
+        <Marker
+          lat={coords.lat}
+          lng={coords.lng}
+          draggable={true}
+          onDragEnd={this.onDragEnd} />
+        <InfoWindow
+          lat={coords.lat}
+          lng={coords.lng}
+          content={'Hello, React :)'}
+          onCloseClick={this.onCloseClick} />
+        <Circle
+          lat={coords.lat}
+          lng={coords.lng}
+          radius={500}
+          onClick={this.onClick} />
+      </Gmaps>
+    );
+  }
+
+});
+
+export default GoogleMap;
