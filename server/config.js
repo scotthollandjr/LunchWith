be107b5d72981c3f58f502945e6ae55e420456d3
Kyle <wolfsonk@gmail.com>
2016-09-12 11:32:29 -0700

    moar heroku tweaks 6

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  databaseURL: "postgres://pricdnjbkgcqgv:-IsFp-pWkCWPppo45CSn2IA42z@ec2-23-23-76-90.compute-1.amazonaws.com:5432/d9urkbhh7o3gm8" || 'postgres://localhost:5432/lunchwith',
+  databaseURL: "postgres://pricdnjbkgcqgv:-IsFp-pWkCWPppo45CSn2IA42z@ec2-23-23-76-90.compute-1.amazonaws.com:5432/d9urkbhh7o3gm8",
   linkedin: {
     clientID: "78b3ua1u1ptbbj",
     clientSecret: "TN6C4QGvwiY5mIS1",
