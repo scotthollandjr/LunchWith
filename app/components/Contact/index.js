af956cc994d52c60fa77ba3528a83c687dd66227
Scott Holland <scotthollandjr@gmail.com>
2016-09-22 17:55:14 -0700

    Add super basic template to privacy and contact pages, removed unneeded about page

--- app/components/Contact/index.js
+++ app/components/Contact/index.js
@@ -8,7 +8,39 @@ class Contact extends React.Component {
         return (
           <div>
             <PageHeader />
-            <h1 className="title is-1 white">Contact</h1>
+            <div id="contact-div" className="tile is-6">
+              <article className="tile is-child notification">
+                <nav className="panel">
+                  <p className="panel-heading">
+                    Contact us:
+                  </p>
+                  <a className="panel-block" href="mailto:info@lunchwith.co">
+                    <span className="icon contact-icon">
+                      <i className="fa fa-envelope"></i>
+                    </span>
+                    info@lunchwith.co
+                  </a>
+                  <a className="panel-block" href="#">
+                    <span className="icon contact-icon">
+                      <i className="fa fa-twitter"></i>
+                    </span>
+                    @LunchWith
+                  </a>
+                  <a className="panel-block" href="#">
+                    <span className="icon contact-icon">
+                      <i className="fa fa-facebook"></i>
+                    </span>
+                    @LunchWith
+                  </a>
+                  <a className="panel-block" href="https://github.com/eyecuelab">
+                    <span className="icon contact-icon">
+                      <i className="fa fa-github"></i>
+                    </span>
+                    @EyeCueLab
+                  </a>
+                </nav>
+              </article>
+            </div>
             <PageFooter />
           </div>
         );
